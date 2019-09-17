5b74b0587fc265ee3ac3c4339d7103829b5efbaa
sunroyi <sunroyi@hotmail.co.jp>
2019-09-17 20:45:26 +0800

    1

--- src/main/java/sun/controller/Interface1.java
+++ src/main/java/sun/controller/Interface1.java
@@ -4,4 +4,6 @@ public interface Interface1 {
     public static final String color = "Blue";
 
     public void drive();
+
+    public void open();
 }
--- src/main/java/sun/controller/InterfaceTest.java
+++ src/main/java/sun/controller/InterfaceTest.java
@@ -6,6 +6,11 @@ public class InterfaceTest extends AbstractClass implements Interface1, Interfac
         System.out.println("Drive car of " + Interface1.color);
     }
 
+    @Override
+    public void open() {
+        System.out.println("Open car of " + Interface1.color);
+    }
+
     public void drive2(){
         System.out.println("Drive car of " + Interface2.color);
     }
@@ -17,6 +22,7 @@ public class InterfaceTest extends AbstractClass implements Interface1, Interfac
     public static void main(String[] args){
         InterfaceTest it = new InterfaceTest();
         it.drive();
+        it.open();
         it.drive2();
         it.showMessage();
     }
