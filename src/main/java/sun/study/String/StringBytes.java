803075f113e7e5714bc4b4df6a048a55e3c6c3a2
Sun.wenli <sunroyi@hotmail.co.jp>
2019-10-27 10:00:17 +0800

    1

--- src/main/java/sun/study/String/StringBytes.java
+++ src/main/java/sun/study/String/StringBytes.java
@@ -8,15 +8,22 @@ public class StringBytes {
 
         String strTest = "我的名字是Royi";
 
+        System.out.println("-------------byte[]-------------");
         byte[] byteTest = strTest.getBytes();
-
         for(byte b:byteTest)
             System.out.println(b);
 
-        System.out.println("--------------------------");
-
+        System.out.println("-------------↓-------------");
         String strChange = new String(byteTest);
-
         System.out.println(strChange);
+
+        System.out.println("-------------char[]-------------");
+        char[] charTest = strTest.toCharArray();
+        for(char c:charTest)
+            System.out.println(c);
+
+        System.out.println("-------------↓-------------");
+        String strChar = String.valueOf(charTest);
+        System.out.println(strChar);
     }
 }
